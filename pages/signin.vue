eb2815dcc907fc06752f5f0cc3b5018b57972d9e
Hampus Fridholm <hampus.fridholm@gmail.com>
2024-01-04 00:42:40 +0100

    Created a simple design for a signup and signin page.

---
+++ pages/signin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="page" class="h-screen flex justify-center items-center bg-gray-100">
+    <div class="h-fit">
+      <h1 class="sign-title">Sign in</h1>
+      <form id="signin-form">
+        <div class="sign-label-input">
+          <label class="sign-label" for="signin-email">Email</label>
+          <input class="sign-input" id="signin-email" type="email" required />
+        </div>
+        <div class="sign-label-input">
+          <label class="sign-label" for="signin-password">Password</label>
+          <input class="sign-input" id="signin-password" type="password" required />
+        </div>
+        <div class="sign-button-div">
+          <button class="sign-button bg-green-400">Sign in</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
---
+++ pages/signup.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="page" class="h-screen flex justify-center items-center bg-gray-100">
+    <div class="h-fit">
+      <h1 class="sign-title">Sign up</h1>
+      <form id="signup-form">
+        <div class="sign-label-input">
+          <label class="sign-label" for="signup-name">Name</label>
+          <input class="sign-input" id="signup-name" type="name" required/>
+        </div>
+        <div class="sign-label-input">
+          <label class="sign-label" for="signup-email">Email</label>
+          <input class="sign-input" id="signup-email" type="email" required />
+        </div>
+        <div class="sign-label-input">
+          <label class="sign-label" for="signup-password">Password</label>
+          <input class="sign-input" id="signup-password" type="password" required />
+        </div>
+        <div class="sign-label-input">
+          <label class="sign-label" for="signup-confirm">Confirm</label>
+          <input class="sign-input" id="signup-confirm" type="password" required />
+        </div>
+        <div class="sign-button-div">
+          <button class="sign-button bg-gray-400">Sign up</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
